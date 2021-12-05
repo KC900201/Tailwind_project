4f404ca46572e65bb180a13bbd3ade782b796261
KC Ng <nkc900201@gmail.com>
2021-12-05 21:06:19 +0900

    (WIP) updating SearchFilters.vue

--- src/components/SearchFilters.vue
+++ src/components/SearchFilters.vue
@@ -1,5 +1,156 @@
-<template></template>
-
+<template>
+  <section class="bg-gray-800 xl:w-72">
+    <div class="flex justify-between px-4 py-3 xl:hidden">
+      <div class="relative max-w-xs w-full">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3">
+          <svg
+            class="h-6 w-6 fill-current text-gray-600"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M16.32 14.9l1.1 1.1c.4-.02.83.13 1.14.44l3 3a1.5 1.5 0 0 1-2.12 2.12l-3-3a1.5 1.5 0 0 1-.44-1.14l-1.1-1.1a8 8 0 1 1 1.41-1.41l.01-.01zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"
+            />
+          </svg>
+        </div>
+      </div>
+      <button
+        type="button"
+        class="
+          ml-4
+          inline-flex
+          items-center
+          hover:bg-gray-600
+          focus:outline-none focus:shadow-outline
+          rounded-lg
+          shadow
+          pl-3
+          pr-4
+        "
+        @click="toggle"
+        :class="[isOpen ? 'bg-gray-600' : 'bg-gray-700']"
+      >
+        <svg
+          class="h-6 w-6 fill-current text-gray-500"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M3 6a1 1 0 0 1 1-1h16a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1zm3 6a1 1 0 0 1 1-1h10a1 1 0 1 1 0 2H7a1 1 0 0 1-1-1zm4 5a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-4z"
+          />
+        </svg>
+        <span class="ml-1 text-white font-medium">Filters</span>
+      </button>
+    </div>
+    <form
+      :class="{ hidden: !isOpen, block: isOpen }"
+      class="xl:block xl:h-full xl:flex xl:flex-col xl:justify-between"
+    >
+      <div class="lg:flex xl:blockl xl:overflow-y-auto">
+        <div
+          class="
+            px-4
+            py-4
+            border-t border-y border-gray-900
+            lg:w-1/3
+            xl:border-t-0 xl:w-full
+          "
+        >
+          <div class="flex flex-wrap -mx-2">
+            <label class="block w-1/2 px-2 sm:w-1/4 lg:w-1/2">
+              <span class="text-sm font-semibold text-gray-500">Bedrooms</span>
+              <select
+                class="
+                  mt-1
+                  form-select
+                  block
+                  w-full
+                  text-white
+                  shadow
+                  focus:bg-gray-600
+                "
+              >
+                <option>4</option>
+              </select>
+            </label>
+            <label class="block w-1/2 px-2 sm:w-1/4 lg:w-1/2">
+              <span class="text-sm font-semibold text-gray-500">Bathrooms</span>
+              <select
+                class="
+                  mt-1
+                  form-select
+                  block
+                  w-full
+                  text-white
+                  shadow
+                  focus:bg-gray-600
+                "
+              >
+                <option>2</option>
+              </select>
+            </label>
+            <label
+              class="mt-4 block w-full px-2 sm:mt-0 sm:w-1/2 lg:mt-4 lg:w-full"
+            >
+              <span class="text-sm font-semibold text-gray-500"
+                >Price Range</span
+              >
+              <select
+                class="
+                  mt-1
+                  form-select
+                  block
+                  w-full
+                  text-white
+                  shadow
+                  focus:bg-gray-600
+                "
+              >
+                <option>Up to $2,000/wk</option>
+              </select>
+            </label>
+          </div>
+        </div>
+        <div
+          class="
+            px-4
+            py-4
+            border-t border-gray-900
+            lg:w-1/3 lg:border-l
+            xl:w-full
+          "
+        >
+          <!--Continue here https://github.com/adamwathan/workcation/blob/master/src/components/SearchFilters.vue -->
+        </div>
+        <div
+          class="
+            px-4
+            py-4
+            border-t border-gray-900
+            lg:w-1/3 lg:border-l
+            xl:w-full
+          "
+        ></div>
+      </div>
+      <div class="bg-gray-900 px-4 py-4 sm:text-right">
+        <button>Update results</button>
+      </div>
+    </form>
+  </section>
+</template>
 <script>
-export default {};
+export default {
+  props: [],
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen;
+    },
+  },
+};
 </script>
